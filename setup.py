5cfa90a434967c32cf0bbaaeb00a778602528a50
ds-wook <leewook94@naver.com>
2021-12-03 10:59:29 +0900

    Update!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyuba",
-    version="0.0.7",
+    version="0.0.8",
     author="Wook Lee",
     author_email="leewook94@gmail.com",
     description="growth hacking tool test",
